b6179743420c227487ebb7f6c8cbae51d4455ca5
sunflower-tc <348885031@qq.com>
2017-02-08 15:42:49 +0800

    test share

--- js/demo.js
+++ js/demo.js
@@ -29,7 +29,7 @@ wx.ready(function () {
     wx.onMenuShareAppMessage({
       title: '互联网之子',
       desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
-      link: 'http://movie.douban.com/subject/25785114/',
+      link: 'http://web.callcloud.com.cn/index.html#/user-reg?loginType=2',
       imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
       trigger: function (res) {
         // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
@@ -45,14 +45,14 @@ wx.ready(function () {
         alert(JSON.stringify(res));
       }
     });
-    alert('已注册获取“发送给朋友”状态事件');
+    alert('触发分享朋友事件');
   };
 
   // 2.2 监听“分享到朋友圈”按钮点击、自定义分享内容及分享结果接口
   document.querySelector('#onMenuShareTimeline').onclick = function () {
     wx.onMenuShareTimeline({
       title: '互联网之子',
-      link: 'http://movie.douban.com/subject/25785114/',
+      link: 'http://web.callcloud.com.cn/index.html#/user-reg?loginType=2',
       imgUrl: 'http://demo.open.weixin.qq.com/jssdk/images/p2166127561.jpg',
       trigger: function (res) {
         // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
@@ -68,7 +68,7 @@ wx.ready(function () {
         alert(JSON.stringify(res));
       }
     });
-    alert('已注册获取“分享到朋友圈”状态事件');
+    alert('触发朋友分享朋友圈事件');
   };
 
   // 2.3 监听“分享到QQ”按钮点击、自定义分享内容及分享结果接口
@@ -96,7 +96,7 @@ wx.ready(function () {
     });
     alert('已注册获取“分享到 QQ”状态事件');
   };
-  
+
   // 2.4 监听“分享到微博”按钮点击、自定义分享内容及分享结果接口
   document.querySelector('#onMenuShareWeibo').onclick = function () {
     wx.onMenuShareWeibo({
